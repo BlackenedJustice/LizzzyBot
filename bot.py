1c400f8a67caabe8872d1153c6b5128f4896a4c6
BlackenedJustice <yrochka109@mail.ru>
2018-02-20 17:43:09 +0300

    Simpple Lisa bot with start and help commands

--- bot.py
+++ bot.py
@@ -6,6 +6,18 @@ from telebot import types
 bot = telebot.TeleBot(config.token)
 
 
+@bot.message_handler(commands=['start'])
+def start_cmd(message):
+    bot.send_message(message.chat.id, '''Привет! Меня зовут Лиза, и меня похители! Я не знаю, где нахожусь! 
+Вокруг только голые, облезлые стены и больше ничего... 
+Я нашла телефон здесь, в контактах только твой номер. Пожалуйста! Помоги!''')
+
+
+@bot.message_handler(commands=['help'])
+def help_cmd(message):
+    bot.send_message(message.chat.id, '''Помоги мне... Пожалуйста...''')
+
+
 @bot.message_handler(content_types=["text"])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
