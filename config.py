4703b1f488bf826e03cb2847e3869e11f7d21e69
BlackenedJustice <yrochka109@mail.ru>
2018-03-13 10:53:13 +0300

    config added

---
+++ config.py
@@ -0,0 +1,9 @@
+token = "533035621:AAEygXdykIMuMAoymN5bkr3F8p9ZWu0PSE4"
+creatorID = 144454876
+permission_error = "У вас недостаточный уровень доступа!"
+registration_error = "Простите, я вас не узнаю.\nПожалуйста, вызовите сначала команду /promote"
+registration_text = 'Введите токен (вы можете получить его у админов)'
+token_error = "Неверный токен!"
+set_name_text = 'Пожалуйста, введите свое имя'
+unknown_command_error = 'Неизвестная команда... Попробуйте еще раз'
+set_name_error = 'Неправильный формат имени!'
